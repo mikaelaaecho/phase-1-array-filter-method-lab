bcf8545efd5eb43b6bbddc875e6d3373e983c97e
mikaela echo <mikaelaaecho@gmail.com>
2023-08-23 13:12:06 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+function findMatching(drivers, search_string) {
+    const matchingDrivers = drivers.filter(driver =>
+        driver.toLowerCase().includes(search_string.toLowerCase())
+    );
+    return matchingDrivers;
+}
+function fuzzyMatch(drivers, query_string) {
+    const matchingDrivers = drivers.filter(drivers =>
+       drivers.toLowerCase().startsWith(query_string.toLowerCase())
+       );
+       return matchingDrivers;
+}
+function matchName(driverObjects, search_name) {
+    const matchingDrivers = driverObjects.filter(driver =>
+        driver.name.toLowerCase() === search_name.toLowerCase()
+        );
+        return matchingDrivers;
+}
